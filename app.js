ba64beb1f7d9b7c2ee34607c1498501d5f354500
Ronny Valdivieso <ron_251@hotmail.com>
2018-05-30 08:23:51 -0500

    Functions

--- app.js
+++ app.js
@@ -1,2 +1,48 @@
 "use strict";
-console.log("It works!");
+// String
+var myName = "Ronny";
+// myName = 20;
+// Number (Integer, Float)
+var myAge = 25;
+// myAge = 'Ronny'
+// Boolean
+var hasHobbies = false;
+// hasHobbies = 1;
+//  Assign types
+var myRealAge;
+myRealAge = 25;
+// myRealAge = '25';
+//  Array
+var hobbies = ["Music", "Programming"];
+hobbies = [100];
+// hobbies = 100;
+// Tuples
+var address = ["Superstreet", 99];
+// Enum
+var Color;
+(function (Color) {
+    Color[Color["Gray"] = 0] = "Gray";
+    Color[Color["Green"] = 100] = "Green";
+    Color[Color["Blue"] = 2] = "Blue"; // 2
+})(Color || (Color = {}));
+var myColor = Color.Blue;
+console.log(myColor);
+// Any
+var car = "BMW";
+console.log(car);
+car = { brand: "BMW", series: 3 };
+console.log(car);
+// Functions
+function returnMyName() {
+    return myName;
+}
+console.log(returnMyName());
+// Void
+function sayHello() {
+    console.log("Hello!");
+}
+// Argument types
+function multiply(value1, value2) {
+    return value1 * value2;
+}
+console.log(multiply(2, 10));
--- app.ts
+++ app.ts
@@ -1,1 +1,64 @@
-console.log("It works!");
+// String
+let myName: string = "Ronny";
+// myName = 20;
+
+// Number (Integer, Float)
+let myAge: Number = 25;
+// myAge = 'Ronny'
+
+// Boolean
+let hasHobbies: boolean = false;
+// hasHobbies = 1;
+
+//  Assign types
+let myRealAge: number;
+myRealAge = 25;
+// myRealAge = '25';
+
+//  Array
+let hobbies: any[] = ["Music", "Programming"];
+hobbies = [100];
+// hobbies = 100;
+
+// Tuples
+let address: [string, number] = ["Superstreet", 99];
+
+// Enum
+enum Color {
+	Gray,	// 0
+	Green = 100, // 100
+	Blue = 2	// 2
+}
+let myColor: Color = Color.Blue;
+console.log(myColor);
+
+// Any
+let car: any = "BMW";
+console.log(car);
+car = { brand: "BMW", series: 3 };
+console.log(car);
+
+// Functions
+function returnMyName(): string {
+	return myName;
+}
+
+console.log(returnMyName());
+
+// Void
+function sayHello(): void {
+	console.log("Hello!");
+}
+
+// Argument types
+function multiply(value1: number, value2: number): number {
+	return value1 * value2;
+}
+console.log(multiply(2, 10));
+
+//  Funtions types
+let myMultiply;
+myMultiply = sayHello;
+myMultiply();
+myMultiply = multiply;
+myMultiply();
